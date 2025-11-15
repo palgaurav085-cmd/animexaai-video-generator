319323a80632e526095ce6e87546549613e3f4f8
palgaurav085-cmd <palgaurav085@gmail.com>
2025-11-16 03:41:16 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{padding: 40, fontSize: 24}}>
-      <h1>Animexa AI — Video Generator</h1>
-      <p>Your Next.js setup is working successfully.</p>
-    </div>
-  );
-}
