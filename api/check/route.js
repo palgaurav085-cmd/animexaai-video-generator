5e72b4ff55e8129850f98277fec3a8fb2f63f5e1
palgaurav085-cmd <palgaurav085@gmail.com>
2025-11-20 02:51:03 +0530

    Update route.js

--- api/check/route.js
+++ api/check/route.js
@@ -1,12 +1,16 @@
 export async function POST(req) {
   const { id } = await req.json();
 
-  const res = await fetch("https://animexa-worker.onrender.com/check", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ id }),
-  });
+  const check = await fetch(
+    `https://animexa-worker.palgaurav085.workers.dev/jobs/${id}`,
+    {
+      headers: {
+        "x-worker-secret": process.env.WORKER_SECRET,
+      },
+    }
+  );
 
-  const data = await res.json();
+  const data = await check.json();
+
   return Response.json(data);
 }
