851a7b0e033cbc0eba24137abcd02b8754b13246
palgaurav085-cmd <palgaurav085@gmail.com>
2025-11-20 01:51:06 +0530

    Update route.js

--- app/api/generate/route.js
+++ app/api/generate/route.js
@@ -1,10 +1,10 @@
 export async function POST(req) {
   const { prompt } = await req.json();
 
-  const res = await fetch("https://animexa-worker.onrender.com/generate", {
+  const res = await fetch("https://animexa-worker.palgaurav085.workers.dev", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ prompt }),
+    body: JSON.stringify({ script: prompt })
   });
 
   const data = await res.json();
